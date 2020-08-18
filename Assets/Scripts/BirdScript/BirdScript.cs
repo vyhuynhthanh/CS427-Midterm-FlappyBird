2cef405a535d506a4c43046bb27d48219f8c42f0
htvy99 <htvy@apcs.vn>
2020-08-19 00:20:09 +0700

    Gameplay scene - BirdScript - CameraScript

---
+++ Assets/Scripts/BirdScript/BirdScript.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BirdScript : MonoBehaviour
+{
+    //to create an instance of our script
+    public static BirdScript instance;
+
+    [SerializeField]
+    private Rigidbody2D myRigidBody;
+    [SerializeField]
+    private Animator anim;
+
+    private float forwardSpeed = 3f;
+    private float bounceSpeed = 4f;
+    private bool didFlap;
+
+    public bool isAlive;
+
+    private Button flapButton;
+
+    void Awake()
+    {
+        //This will slow our game if we have many getComponet
+        //myRigidBody = GetComponent<Rigidbody2D>();
+
+        //If we don't have a script pointing to this instance
+        if (instance == null)
+        {
+            instance = this;
+        }
+        isAlive = true;
+
+        //get the button component of the FlapButton object
+        flapButton = GameObject.FindGameObjectWithTag("FlapButton").GetComponent<Button>();
+        //Add the listener to the button: so that when we touch the screen, the bird is flapping and fly (for every bird object)
+        flapButton.onClick.AddListener(() => FlapTheBird());
+
+        SetCameraX();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // FixedUpdate is called every 2 or 3 frames
+    void FixedUpdate()
+    {
+        //if our bird is alive => move it to the right
+        if (isAlive)
+        {
+            //get the current position
+            Vector3 temp = transform.position;
+            temp.x += forwardSpeed * Time.deltaTime;
+            transform.position = temp;
+        }
+        //if our bird need to flap
+        if (didFlap)
+        {
+            //the bird flap once every time we touch the screen
+            //if the bird is already flap, we stop
+            didFlap = false;
+            myRigidBody.velocity = new Vector2(0, bounceSpeed);
+            anim.SetTrigger("Flap");
+        }
+    }
+
+    void SetCameraX()
+    {
+        //the position of the camera minus the position of our bird
+        CameraScript.offsetX = (Camera.main.transform.position.x - transform.position.x) - 1f;
+    }
+
+    public float GetPositionX()
+    {
+        return transform.position.x;
+    }
+
+    public void FlapTheBird()
+    {
+        didFlap = true;
+    }
+}
---
+++ Assets/Scripts/CameraScript/CameraScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    //this offset is calculated in the birdscript
+    public static float offsetX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the bird exists in the game
+        if(BirdScript.instance != null)
+        {
+            //if the bird is alive, the camera will follow the bird
+            if (BirdScript.instance.isAlive)
+            {
+                MoveTheCamera();
+            }
+        }
+    }
+
+    void MoveTheCamera()
+    {
+        Vector3 temp = transform.position;
+        temp.x = BirdScript.instance.GetPositionX() + offsetX;
+        transform.position = temp;
+    }
+}

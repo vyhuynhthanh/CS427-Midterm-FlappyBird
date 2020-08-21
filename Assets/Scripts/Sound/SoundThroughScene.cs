bf50097a05c8eecf2da249e01d7fe57c99018172
htvy99 <htvy@apcs.vn>
2020-08-21 21:40:39 +0700

    Finish

---
+++ Assets/Scripts/Sound/SoundThroughScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundThroughScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    //Play Global
+    private static SoundThroughScene instance = null;
+    public static SoundThroughScene Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance !=null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
